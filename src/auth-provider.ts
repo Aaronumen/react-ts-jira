a43e9d338750d38ade5b284230a0dd44cbb55a97
aaronumen <1076552287@qq.com>
2021-05-20 19:03:54 +0800

    fix: 第四次

---
+++ src/auth-provider.ts
@@ -0,0 +1,44 @@
+// 在真实环境中，如果使用firebase这种第三方auth服务的话，本文件不需要开发者开发
+
+import {User} from './screens/project-list/search-panel';
+
+const localStorageKey = '__auth_provider_token__'
+const apiUrl = process.env.REACT_APP_API_URL
+export const getToken = () => window.localStorage.getItem(localStorageKey)
+
+export const handleUserResponse = ({user}: { user: User }) => {
+    window.localStorage.setItem(localStorageKey, user.token || '')
+    return user
+}
+export const login = (data: { username: string, password: string }) => {
+    return fetch(`${apiUrl}/login`, {
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        method: 'POST',
+        body: JSON.stringify(data)
+    }).then(async res => {
+        if (res.ok) {
+            return handleUserResponse(await res.json())
+        } else {
+            return Promise.reject(data)
+        }
+    })
+}
+export const register = (data: { username: string, password: string }) => {
+    return fetch(`${apiUrl}/register`, {
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        method: 'POST',
+        body: JSON.stringify(data)
+    }).then(async res => {
+        if (res.ok) {
+            return handleUserResponse(await res.json())
+        } else {
+            return Promise.reject(data)
+        }
+    })
+}
+
+export const logout = async () => window.localStorage.removeItem(localStorageKey)
